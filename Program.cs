75a873f42b0d8824d1228806d7f5364cc3aa5aec
Jeremiah Diboll <jdiboll@gmail.com>
2017-08-14 10:09:39 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Our code goes here
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
